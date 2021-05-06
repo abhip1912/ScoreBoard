50c0eea8eac995967fe6c09ed9dbce9ed36572a7
khunt jenish <jenishkhunt27@gmail.com>
2021-05-06 14:02:26 +0530

    complete few point.

--- lib/page/model/model.dart
+++ lib/page/model/model.dart
@@ -1,13 +1,15 @@
+import 'package:get/get.dart';
+
 class UserModel {
   String name;
-  int score;
+  var score = '0'.obs;
 
   UserModel({this.name, this.score});
 
   factory UserModel.fromjson(Map<String, dynamic> json) {
     return UserModel(
       name: json['name'] as String,
-      score: json['score'] as int,
+      score: json['score'] as RxString,
     );
   }
 
